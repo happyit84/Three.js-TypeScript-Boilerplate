fd6e648feb8f99ca478a8b23a5417dc9b756d39b
yeonsik <happyit84@gmail.com>
2022-05-08 06:20:28 +0900

    extract geometry from gltf file

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,5 +1,11 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
+//import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
+//import * as path from 'path'
+//import { Mesh } from 'three'
+import {Mutex} from 'async-mutex'
+
 
 const scene = new THREE.Scene()
 
@@ -12,35 +18,72 @@ document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
 
-const geometry = new THREE.BoxGeometry()
+//var _geometry = new THREE.TorusGeometry(0.5)
+var _geometry: any;
 const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
+    color: 0x00ff00,    
     wireframe: true,
 })
-
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
-
-window.addEventListener('resize', onWindowResize, false)
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
-}
-
-function animate() {
-    requestAnimationFrame(animate)
-
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
-
-    controls.update()
-
-    render()
-}
-
-function render() {
-    renderer.render(scene, camera)
-}
-animate()
+console.log("TEST0")
+const gltfLoader = new GLTFLoader()
+const mutex =  new Mutex()
+const mutex2 = new Mutex()
+//const release1 = await mutex2.acquire();
+    //gltfLoader.load(path.join(__dirname, 'torus.gltf'), (gltf) => {
+mutex2.acquire().then((release1)=>{
+    gltfLoader.load('torus.gltf', (gltf) => {
+        console.log("TEST1")
+        gltf.scene.traverse((child) => {
+            child.traverse((mesh) => {            
+                if (mesh instanceof THREE.Mesh)
+                {
+                    mutex.runExclusive(() => {
+                        //if (undefined == _geometry)
+    //                      _geometry = mesh.geometry
+                    })
+                    //mesh.geometry
+                    console.log(mesh.geometry)
+                    console.log(typeof mesh.geometry)
+                    _geometry = mesh.geometry
+                    release1()
+                }
+            })
+            /*console.log("TEST2")
+            console.log(JSON.stringify(child))
+            console.log(child.type);
+            if (child.type == "Mesh")
+                console.log("object type is Mesh");
+            _geometry = child.getObjectByName("geometries");
+            console.log(_geometry)*/
+        })    
+    })    
+})
+//mutex2.acquire().then((release) => {
+mutex2.waitForUnlock().then(()=>{  
+    const cube = new THREE.Mesh(_geometry, material)
+    scene.add(cube)
+    
+    window.addEventListener('resize', onWindowResize, false)
+    function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight
+        camera.updateProjectionMatrix()
+        renderer.setSize(window.innerWidth, window.innerHeight)
+        render()
+    }
+    
+    function animate() {
+        requestAnimationFrame(animate)
+    
+        cube.rotation.x += 0.01
+        cube.rotation.y += 0.01
+    
+        controls.update()
+    
+        render()
+    }
+    
+    function render() {
+        renderer.render(scene, camera)
+    }
+    animate()
+})
