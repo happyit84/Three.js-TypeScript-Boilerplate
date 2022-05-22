aa506789401f9d35837fe182d693b729c2b1df10
yeonsik <happyit84@gmail.com>
2022-05-22 22:13:44 +0900

    add triangle to geometry of THREE object

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,3 +1,5 @@
+//import * as fs from 'fs';
+//const fs = require('fs')
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
@@ -5,6 +7,7 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 //import * as path from 'path'
 //import { Mesh } from 'three'
 import {Mutex} from 'async-mutex'
+
 
 
 const scene = new THREE.Scene()
@@ -20,6 +23,9 @@ const controls = new OrbitControls(camera, renderer.domElement)
 
 //var _geometry = new THREE.TorusGeometry(0.5)
 var _geometry: any;
+
+
+
 const material = new THREE.MeshBasicMaterial({
     color: 0x00ff00,    
     wireframe: true,
@@ -31,8 +37,9 @@ const mutex2 = new Mutex()
 //const release1 = await mutex2.acquire();
     //gltfLoader.load(path.join(__dirname, 'torus.gltf'), (gltf) => {
 mutex2.acquire().then((release1)=>{
-    gltfLoader.load('torus.gltf', (gltf) => {
+    gltfLoader.load('square.gltf', (gltf) => {
         console.log("TEST1")
+        console.log(gltf)
         gltf.scene.traverse((child) => {
             child.traverse((mesh) => {            
                 if (mesh instanceof THREE.Mesh)
@@ -45,6 +52,29 @@ mutex2.acquire().then((release1)=>{
                     console.log(mesh.geometry)
                     console.log(typeof mesh.geometry)
                     _geometry = mesh.geometry
+
+                    //fs.writeFileSync("geometry.json", JSON.stringify(_geometry), "utf8")
+                    //console.log("Data written successfully to disk")
+
+                    //var pos_arr = _geometry.attributes.position.array
+                    //console.log(pos_arr)
+                    _geometry.attributes.position.array = new Float32Array([
+                        -1,                        0,                        1,
+                        1,                        0,                        1,
+                        -1,                        0,                        -1,
+                        1,                        0,                        -1,
+                        2, 0, 0,
+                        0,3,0,
+                        0,0,4
+                    ]);
+
+                    _geometry.index.array = new Uint16Array([
+                        0,1,3,
+                        0,3,2,
+                        //0,3,4,
+                        4,5,6
+                    ]);
+
                     release1()
                 }
             })
